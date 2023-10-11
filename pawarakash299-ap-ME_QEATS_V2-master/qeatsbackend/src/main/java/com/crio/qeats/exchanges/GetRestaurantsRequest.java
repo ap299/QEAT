b6ea8f9c6e788d601adec9ff8e7fd04bd45dec7a
Akash Pawar <34743298+ap299@users.noreply.github.com>
2023-10-11 15:02:56 +0530

    Add files via upload

---
+++ pawarakash299-ap-ME_QEATS_V2-master/qeatsbackend/src/main/java/com/crio/qeats/exchanges/GetRestaurantsRequest.java
@@ -0,0 +1,24 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.exchanges;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import org.springframework.lang.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GetRestaurantsRequest {public GetRestaurantsRequest(double d, double e) {}
+
+
+
+}
+
